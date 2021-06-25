f15a90e5905f9ad72ac0548bcaf0a497546e4c10
Dheerajkumar1818 <54733867+Dheerajkumar1818@users.noreply.github.com>
2021-06-25 09:00:32 +0530

    Initial commit

---
+++ src/components/Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>WELCOME TO MY FIRST WEBSITE </h1>
+      <p>Which i learnt from ShapeAI Bootcamp </p>
+    </div>
+  );
+}
+export default Info;
